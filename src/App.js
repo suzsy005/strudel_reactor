1d0b168b43da79467f7b0c1b658958f61fb8e867
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-05 00:38:32 +1030

    Checking the how strudel works and so on

--- src/App.js
+++ src/App.js
@@ -121,11 +121,11 @@ return (
                     <div className="col-md-4">
 
                         <nav>
-                            <button id="process" className="btn btn-outline-primary">Preprocess</button>
-                            <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+                            <button id="process" className="btn btn-outline-primary m-1">Preprocess</button>
+                            <button id="process_play" className="btn btn-outline-secondary m-1">Proc & Play</button>
                             <br />
-                            <button id="play" className="btn btn-outline-primary">Play</button>
-                            <button id="stop" className="btn btn-outline-primary">Stop</button>
+                            <button id="play" className="btn btn-outline-primary m-1">Play</button>
+                            <button id="stop" className="btn btn-outline-danger m-1">Stop</button>
                         </nav>
                     </div>
                 </div>
