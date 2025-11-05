4ef9070d0e82099e0f4ec56b14dbe42b964083d4
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-05 20:28:51 +1030

    Updated PlayButtons
    in order to make it easy to track of these buttons, wrap these in button
    group

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -2,8 +2,11 @@ function PlayButtons() {
 	
 	return (
 		<>
-			<button id="play" className="btn btn-outline-primary m-1">Play</button>
-			<button id="stop" className="btn btn-outline-danger m-1">Stop</button>
+			{/* wrap these buttons in button group to make it easy to track of these buttons */} 
+			<div className="btn-group" role="group" aria-label="Basic mixed styles example">
+				<button id="play" className="btn btn-outline-primary m-1">Play</button>
+				<button id="stop" className="btn btn-outline-danger m-1">Stop</button>
+			</div>
 		</>
 	);
 }
