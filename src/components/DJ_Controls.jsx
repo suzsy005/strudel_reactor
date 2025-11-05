d37bb8bd412744357907f458853999a4ae91f596
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-05 23:53:34 +1030

    Added valume range bar feature to change the valume of music
    I found that I can't see the work on my browser screeen. Right when I
    press the reload button, I can see the work in a second. But after that
    I can't see anything on my screen. I'll figute this out later

--- src/components/DJ_Controls.jsx
+++ src/components/DJ_Controls.jsx
@@ -3,9 +3,15 @@ function DJ_Controls(){
 	return (
 		<>
 			<div className="input-group mb-3">
-			  <span className="input-group-text" id="cpm_label">setCPM</span>
-			  <input type="text" className="form-control" placeholder="120" aria-label="Username" aria-describedby="cpm_label" />
+				{/* input feature to change the speed of music(CPM) */}
+				<span className="input-group-text" id="cpm_label">setCPM</span>
+			  	<input type="text" className="form-control" id="cpm_text_input "placeholder="120" aria-label="cpm" aria-describedby="cpm_label" />
 			</div>
+			
+			{/* valume range bar feature to change the valume of music */}		  
+			<label for="valume_range" className="form-label">Valume</label>
+			<input type="range" className="form-range" min="0" max="1" step="0.05" id="valume_range" />
+
 		</>
 	); 
 }
