f700bf06983eaa13f50ef9b97c25cd51bd3d9749
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-06 17:14:53 +1030

    Fixed error that Play button doesn't play!
    I put onClick={onPlay} inside of className sectoin, put onClick={onStop}
    as well, but Fixed!

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -4,8 +4,8 @@ function PlayButtons({ onPlay, onStop }) {
 		<>
 			{/* wrap these buttons in button group to make it easy to track of these buttons */} 
 			<div className="btn-group" role="group" aria-label="Basic mixed styles example">
-				<button id="play" className="btn btn-outline-primary onClick={onPlay} m-1">Play</button>
-				<button id="stop" className="btn btn-outline-danger onClick={onStop} m-1">Stop</button>
+				<button id="play" className="btn btn-outline-primary m-1" onClick={onPlay}>Play</button>
+				<button id="stop" className="btn btn-outline-danger m-1" onClick={onStop}>Stop</button>
 			</div>
 		</>
 	);
--- src/components/ProcButtons.jsx
+++ src/components/ProcButtons.jsx
@@ -11,4 +11,7 @@ function ProcButtons() {
 	);
 }
 
+
+
+
 export default ProcButtons;
