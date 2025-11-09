91e81028ac9aced2ee9dce37c35f6ddc28be0b7d
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-09 23:46:47 +1030

    Added more features for songs 10 sec skips

--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -12,6 +12,16 @@ function PlayButtons({ onPlay, onStop }) {
 						<i className="bi bi-stop-circle"></i>
 					</button>
 				</div>
+				/* these are for skip/back to song */
+				<div className="row justify-content-evenly mt-2">
+					<button id="next" className="btn btn-outline-secondary m-1 col-2" onClick={onPlay}>
+						<i className="bi bi-chevron-left"></i>
+					</button>
+					<button id="back" className="btn btn-outline-secondary m-1 col-2" onClick={onPlay}>
+						<i className="bi bi-chevron-right"></i>
+					</button>
+				</div>
+				/* these are for 10 skip/go back song */
 				<div className="row justify-content-evenly mt-2">
 					<button id="next" className="btn btn-outline-secondary m-1 col-2" onClick={onPlay}>
 						<i className="bi bi-chevron-left"></i>
