f18fb43e32dd36fd79aa3900c34058d3f06d733c
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-11 00:35:52 +1030

    Added regex for word matches
    This was really hard one, but got to understand by watching recording

--- src/utils/PreprocessLogic.jsx
+++ src/utils/PreprocessLogic.jsx
@@ -2,7 +2,10 @@ export function Preprocess ({ inputText, volume }) {
 	
 	outputText = outputText.replaceAll("{VOLUME}", volume)
 	
-	// let regex = 
+	// use regex here to look for word matches
+	let regex = /[a-zA-Z0-9_+:\s*\n[\s\S]+?\r?\n(?=[a-zA-Z0-9_]*[:\/])/gm;
+	
+	
 	
 
 	
