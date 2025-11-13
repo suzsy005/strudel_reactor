63f8e1ab743576ccb6733cf58e595a25a99401b0
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-13 22:43:35 +1030

    Defined onVolumeChange function in DJ_Controls.jsx
    Although this function is used in App.js, wasn't defined in DJ_Controls

--- src/components/DJ_Controls.jsx
+++ src/components/DJ_Controls.jsx
@@ -1,4 +1,4 @@
-function DJ_Controls({ cpm, onCpmChange }){
+function DJ_Controls({ cpm, onCpmChange, onVolumeChange }){
 	
 	// take input value to App.js when users type CPM
 	const handleCpmInputChange = (event) => {
