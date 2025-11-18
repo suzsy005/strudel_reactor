6443f442e60c2db516e6cd836f476c52aa84bb5c
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-18 19:58:47 +1030

    Updated index.js

---
+++ src/tunes/index.js
@@ -0,0 +1,7 @@
+import { stranger_tune } from "./src/tunes";
+import { tune2 } from "./tune2";
+
+export const tunes = [
+  { title: "Stranger Tune", code: stranger_tune },
+  { title: "Tune 2", code: tune2 }
+];
